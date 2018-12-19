a4ecf9252d691c388632aeebeb87b120190bf62b
Highnewb <thouveninvalentin@gmail.com>
2018-12-19 17:16:36 +0100

    Modif HumanPlayer

--- src/model/HumanPlayer.java
+++ src/model/HumanPlayer.java
@@ -2,6 +2,7 @@ package model;
 
 import model.Boat;
 import model.Board;
+import model.Position;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,13 @@ public class HumanPlayer {
     int nbMissShot = 0;
 	int nbHitShot = 0;
 
+	public HumanPlayer(Board b, ArrayList<Boat>boatList){
+		board=b;
+		this.boatList= boatList;
+		missTileList=new ArrayList<Position>();
+		hitTileList=new ArrayList<Position>();
+	}
+	
 	
     public int getNbMissShot() {
 		return nbMissShot;
@@ -47,24 +55,79 @@ public class HumanPlayer {
 		this.missTileList = missTileList;
 	}
 
-	public HumanPlayer(Board b, ArrayList<Boat>boatList){
-		board=b;
-		this.boatList= boatList;
-		missTileList=new ArrayList<Position>();
-		hitTileList=new ArrayList<Position>();
-	}
+
 
     public boolean lost(){
-        boolean reponse = false;
-        return reponse;
+    	boolean lost = true;
+
+    	for (int i = 0; i < boatList.size(); i++) {
+    		if (boatList.get(i).getHealth() > 0) {
+    			lost = false;
+    			break;
+    		}
+    	}
+
+    	return lost;
     }
 
-    public boolean tileTouched(){
-        boolean reponse = false;
-        return reponse;
+    public boolean tileTouched(Position p){
+    	return (missTileList.contains(p) || hitTileList.contains(p));
     }
 
-    public void gettingShot(Position p){}
+    public void gettingShot(Position p){
+    	if(board.checkBoatPresence(p)){
+    		int indiceB=this.searchBoat(p);
+    		assert(indiceB!=-1);
+    		if(boatList.get(indiceB).decreaseHealth()){
+    			board.sinkBoat(boatList.get(indiceB),hitTileList);
+
+    		}else{
+    			board.hit(p);
+    			hitTileList.add(p);
+    		}
+
+    		nbHitShot++;
+
+    	}else{
+    		missTileList.add(p);
+    		nbMissShot++;
+    	}
+    }
+    
+    public int searchBoat(Position p){
+		boolean found=false;
+		Position pos;
+		int size;
+		int i=-1;
+		while (!found && i<boatList.size()-1){
+			i++;
+			pos=boatList.get(i).getPos();
+			size=boatList.get(i).getSize();
+			boolean orientation=boatList.get(i).getOrientation();
+			if(orientation){
+				for(int l=0;l<size;l++){
+					Position tempY=new Position(pos);	
+					tempY.setY(tempY.getY()+l);
+					if(p.compareTo(tempY)==0){
+						found=true;
+					}
+				}
+			}else{
+				for(int l=0;l<size;l++){
+					Position tempX=new Position(pos);
+					tempX.setX(tempX.getX()+l);
+					if(p.compareTo(tempX)==0){
+						found=true;
+					}
+				}
+			}
+		}
+		if(found){
+			return i;
+		}else{
+			return -1;
+		}
+    }
 
     public Board getBoard() {
         return board;
