05611d831cf2b4b97d96993a48ab5c2859bb9c34
Highnewb <thouveninvalentin@gmail.com>
2018-12-19 16:42:35 +0100

    Modif Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -2,9 +2,9 @@ package model;
 
 import java.util.ArrayList;
 
-import javax.swing.text.Position;
-
 import model.Boat;
+import modele.Bateau;
+import modele.Position;
 
 public class Board {
 	
@@ -12,17 +12,77 @@ public class Board {
 	
     private int[][] board;
 
-    public Board(int[][] plateau) {}
+    public Board(int[][] board){
+		this.board=board;
+	}
     
-    public Board(ArrayList<Boat>listBateau) {}
+    public Board(ArrayList<Boat>boatList) {
+    	board=new int[LINESIZE][LINESIZE];
+		int size;
+		boolean orientation;
+		Position currentPos;
+		
+		for(int i=0;i<boatList.size();i++){
+			size=boatList.get(i).getSize();
+			orientation=boatList.get(i).getOrientation();
+			currentPos=boatList.get(i).getPos();
+			
+			if(orientation){
+				
+				for(int y=currentPos.getY();y<currentPos.getY()+size;y++){
+					if(this.board[currentPos.getX()][y]==0){
+						this.board[currentPos.getX()][y]=1;
+					}else{
+						System.out.println("Erreur : Deux bâteaux ne peuvent pas êtres créés au même endroit");
+					}
+				}
+			}else{
+				//si le bateau est horizontal
+				
+				for(int x=currentPos.getX();x<currentPos.getX()+size;x++){
+					if(this.board[x][currentPos.getY()]==0){
+						this.board[x][currentPos.getY()]=1;
+					}else{
+						System.out.println("Erreur : Deux bâteaux ne peuvent pas êtres créés au même endroit");
+					}
+				}
+			}
+		}
+    }
 
-    public void hit(Position p) {}
+    public void hit(Position p) {
+    	if(this.checkBoatPresence(p)==true){	
+			board[p.getX()][p.getY()]=-1;;
+		}
+    }
 
-    public void sinkBoat(Boat b){}
+    public void sinkBoat(Boat b, ArrayList<Position> hitList){
+    	int size=b.getSize();
+		boolean orientation=b.getOrientation();
+		Position currentPos=b.getPos();
+		if(orientation){
+			for(int y=currentPos.getY();y<currentPos.getY()+size;y++){		
+				this.board[currentPos.getX()][y]=-2;
+				hitList.add(new Position(currentPos.getX(),y));
+			}
+		}else{
+			for(int x=currentPos.getX();x<currentPos.getX()+size;x++){	
+				this.board[x][currentPos.getY()]=-2;
+				hitList.add(new Position(x,currentPos.getY()));
+			}
+		}
+    }
 
     public boolean checkBoatPresence(Position p){
-        boolean reponse = false;
-        return reponse;
+    	boolean b;
+		int v=board[p.getX()][p.getY()];
+
+		if(v==1){
+			b=true;
+		}else{
+			b=false;
+		}
+		return b;
     }
     
 	public int getValue(int x, int y) {
@@ -36,5 +96,18 @@ public class Board {
     public void setBoard(int[][] board) {
         this.board = board;
     }
+    
+    public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int j = 0; j < LINESIZE; j++) {
+			for (int i = 0; i < LINESIZE; i++) {
+				sb.append(board[i][j] + " | ");
+			}
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
 
 }
