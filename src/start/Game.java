8d2f88f9806cb1faf6115273944822125ce2538b
Fornito Marvin <marvin.fornito@gmail.com>
2018-12-20 14:22:23 +0100

    Gestion du lancement du jeu et des vues

--- src/start/Game.java
+++ src/start/Game.java
@@ -1,62 +1,57 @@
 package start;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
-
+import javax.swing.JMenuBar;
+import model.NavalBattle;
+import patternStrategie.RandomShot;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import javax.swing.JPanel;
 import patternDAO.AbstractDAOFactory;
-import model.NavalBattle;
-import model.NavalBattle;
-import views.GameView;
-import views.LoadMenuView;
-import views.MainMenuView;
-import views.NewGameView;
+import patternFactory.EraXVI;
+import views.*;
 
 public class Game extends JFrame {
 
-	private CardLayout cl;
-	private MainMenuView mmv;
-	private LoadMenuView lmv;
-	private NewGameView ngv;
-	private GameView gv;
-	
+	  private CardLayout cardLayout;
+	  private JPanel mainPanel;
+	    
 	public Game() {
 		super("Bataille Navale");
-		setPreferredSize(new Dimension(500,500));
-		cl = new CardLayout();
-		NavalBattle nb = new NavalBattle(AbstractDAOFactory.getAbstractDAOFactory());
-		mmv = new MainMenuView(nb, this);
-		lmv = new LoadMenuView(nb, this);
-		ngv = new NewGameView(nb, this);
-		gv = new GameView(nb, this);
-		add(mmv, cl);
-		add(lmv, cl);
-		add(ngv, cl);
-		add(ngv, cl);
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
-		setVisible(true);
+        NavalBattle battle = new NavalBattle(AbstractDAOFactory.getAbstractDAOFactory());
+        this.buildFrame(battle);
 	}
 	
-	public void changeView(String viewName){
-		switch (viewName){
-		case "game":
-			cl.show(this,gv.getName());
-			break;
-		case "loadMenu" :
-			cl.show(this,lmv.getName());
-			break;
-		case "mainMenu":
-			cl.show(this,mmv.getName());
-			break;
-		case "newGame":
-			cl.show(this,ngv.getName());
-			break;
-		}
+	private void buildFrame(NavalBattle battle) {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(600, 500));
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        this.mainPanel.add(new MainMenuView(this), "menu");
+        this.mainPanel.add(new NewGameView(this,battle), "newGame");
+        this.mainPanel.add(new GameView(this, battle), "game");
+        this.mainPanel.add(new LoadGameView(this, battle), "resumeGame");            
+        this.add(mainPanel);
+        this.pack();
+        this.setVisible(true);
 	}
+	
+	public void switchToPanel(String panelName) {
+        switch (panelName) {
+            case "menu" :
+                cardLayout.show(mainPanel, "menu");
+                break;
+            case "newGame" :
+                cardLayout.show(mainPanel, "newGame");
+                break;
+            case "resumeGame" :
+                cardLayout.show(mainPanel, "resumeGame");
+                break;
+            case "game" :
+                cardLayout.show(mainPanel, "game");
+        }
+    }
 	
 	public static void main(String[] args) {
 		new Game();		
